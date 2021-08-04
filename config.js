de9fb2640b7e314e657738418cfa31e391470861
Abhijeet0001 <64309773+Abhijeet0001@users.noreply.github.com>
2021-08-04 16:55:51 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAA_N_PV-vNAhcldN9AD6pZiFuwYdFEgps",
+    authDomain: "story-telling-app-57b38.firebaseapp.com",
+    databaseURL: "https://story-telling-app-57b38-default-rtdb.firebaseio.com",
+    projectId: "story-telling-app-57b38",
+    storageBucket: "story-telling-app-57b38.appspot.com",
+    messagingSenderId: "153601728634",
+    appId: "1:153601728634:web:b54b10875059ec6c951bc3"
+  };
